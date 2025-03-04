ad001043b3ace8ef6062a414ea5f85b35fcd3c00
anaggaz <alae-eddine.naggaz@etu.u-bordeaux.fr>
2025-03-04 16:05:56 +0100

    front 2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,17 @@
 import HelloWorld from './components/HelloWorld.vue'
 import ImagesSlider from './components/ImagesSlider.vue';
 import ImageUploader from './components/ImageUploader.vue';
+import ImageGallery from './components/ImageGallery.vue';
+import ImageDetails from './components/ImageDetails.vue';
+
 </script>
 
 <template>
   <HelloWorld msg="Images App" />
   <ImageUploader/>
   <ImagesSlider/>
+  <ImageGallery/>
+  <ImageDetails/>
 </template>
 
 <style scoped>
