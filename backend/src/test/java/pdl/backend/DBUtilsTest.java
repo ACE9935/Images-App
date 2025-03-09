8cc8084da6d971c6120f1a4557bd47ec2e5bf0e8
Mohammed Amine Rabih <mohammed-amine.rabih@etu.u-bordeaux.fr>
2025-03-10 00:40:19 +0100

    unit test

---
+++ backend/src/test/java/pdl/backend/DBUtilsTest.java
@@ -0,0 +1,41 @@
+package pdl.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class DBUtilsTest {
+
+    @Test
+    void testSerializeDeserialize1D() throws IOException, ClassNotFoundException {
+        int[] original = {1, 2, 3, 4, 5};
+        byte[] serialized = DBUtils.serialize1D(original);
+        int[] deserialized = DBUtils.deserialize1D(serialized);
+        assertArrayEquals(original, deserialized);
+    }
+
+    @Test
+    void testSerializeDeserialize2D() throws IOException, ClassNotFoundException {
+        int[][] original = {{1, 2, 3}, {4, 5, 6}};
+        byte[] serialized = DBUtils.serialize2D(original);
+        int[][] deserialized = DBUtils.deserialize2D(serialized);
+        assertArrayEquals(original, deserialized);
+    }
+
+    @Test
+    void testSerializeDeserialize3D() throws IOException, ClassNotFoundException {
+        int[][][] original = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}};
+        byte[] serialized = DBUtils.serialize3D(original);
+        int[][][] deserialized = DBUtils.deserialize3D(serialized);
+        assertArrayEquals(original, deserialized);
+    }
+
+    @Test
+    void testInvalidDeserialization() {
+        byte[] invalidData = new byte[]{1, 2, 3, 4};
+        assertThrows(IOException.class, () -> DBUtils.deserialize2D(invalidData));
+        assertThrows(IOException.class, () -> DBUtils.deserialize3D(invalidData));
+        assertThrows(IOException.class, () -> DBUtils.deserialize1D(invalidData));
+    }
+}
