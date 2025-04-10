b462481ffed231023543aa68972dc48628443866
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2025-04-11 00:15:54 +0200

    .gitignore update

--- auth-backend/src/app-configurations.ts
+++ auth-backend/src/app-configurations.ts
@@ -2,8 +2,8 @@ export const configurations = {
     appName: "Images App",
     developers:["Anas EL MOUDEN", "Alaa Naggaz", "Mohamed Amine Rabih"],
     bluetheme: "rgb(94, 123, 238)",
-    host: "localhost:5000",
-    clientHost:"http://localhost:5173",
+    host: "https://images-app-backend.vercel.app",
+    clientHost:"https://images-app.vercel.app",
     emailAdress: "noreply@trueflowing.com",
     domain: "trueflowing.com"
   };
