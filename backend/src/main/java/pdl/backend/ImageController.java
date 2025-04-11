7e93a7d0b8733f7e7a2b2ecc106d0500edf0e0cc
Anas El mouden <anas.el-mouden@etu.u-bordeaux.fr>
2025-04-11 13:13:08 +0200

    fix

--- backend/src/main/java/pdl/backend/ImageController.java
+++ backend/src/main/java/pdl/backend/ImageController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class ImageController {
 
